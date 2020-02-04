dbbab5c035e762b53a47256f208c34a89c9b1e7f
Nikolaj Sommer <nikolaj@sommer.gl>
2020-02-04 10:06:18 +0100

    First commit

---
+++ CalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82e14462-fb21-4b14-a4d4-7c7d62807d08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var test = new Calculator.Calculator();
+            var result = test.Add(4, 3);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            var test = new Calculator.Calculator();
+            var result = test.Subract(4, 3);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            var test = new Calculator.Calculator();
+            var result = test.Multiply(4, 4);
+            Assert.AreEqual(16, result);
+        }
+
+
+
+        [TestMethod]
+        public void TestPower()
+        {
+            var test = new Calculator.Calculator();
+            var result = test.Power(2, 4);
+            Assert.AreEqual(16, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestSquareRoot()
+        {
+            var test = new Calculator.Calculator();
+            var result = test.squareroot(-4);
+
+        }
+    }
+}
