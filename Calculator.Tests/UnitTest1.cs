3197a662a772a51616773d88e916a3a9de5ec911
Rasmus Carlsen <rasmus.carlsen@web.de>
2020-02-05 14:52:34 +0100

    Changed to use tabs instead of spaces

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -4,49 +4,48 @@ using NUnit.Framework;
 
 namespace CalculatorUnitTest
 {
-    public class CalculatorTests
-    {
-        [Test]
-        public void TestAdd()
-        {
-            var test = new Calculator.Calculator();
-            var result = test.Add(4, 3);
-            Assert.AreEqual(7, result);
-        }
+	public class CalculatorTests
+	{
+		[TestCase(5,		3,		8, 1.0/1000000)]
+		[TestCase(2.5,		2.5,	5, 1.0/1000000)]
+		[TestCase(1.0/3,	2.0/3,	1, 1.0/1000000)]
+		public void TestAdd(double a, double b, double res, double theta)
+		{
+			var test = new Calculator.Calculator();
+			var result = test.Add(a, b);
+			Assert.AreEqual(res, result, theta);
+		}
 
-        [Test]
-        public void TestSubtract()
-        {
-            var test = new Calculator.Calculator();
-            var result = test.Subract(4, 3);
-            Assert.AreEqual(1, result);
-        }
+		[Test]
+		public void TestSubtract()
+		{
+			var test = new Calculator.Calculator();
+			var result = test.Subract(4, 3);
+			Assert.AreEqual(1, result);
+		}
 
-        [Test]
-        public void TestMultiply()
-        {
-            var test = new Calculator.Calculator();
-            var result = test.Multiply(4, 4);
-            Assert.AreEqual(16, result);
-        }
+		[Test]
+		public void TestMultiply()
+		{
+			var test = new Calculator.Calculator();
+			var result = test.Multiply(4, 4);
+			Assert.AreEqual(16, result);
+		}
 
+		[Test]
+		public void TestPower()
+		{
+			var test = new Calculator.Calculator();
+			var result = test.Power(2, 4);
+			Assert.AreEqual(16, result);
+		}
 
+		[Test]
+		public void TestSquareRoot()
+		{
+			var test = new Calculator.Calculator();
+			Assert.Throws<InvalidOperationException>(() => test.squareroot(-4));
 
-        [Test]
-        public void TestPower()
-        {
-            var test = new Calculator.Calculator();
-            var result = test.Power(2, 4);
-            Assert.AreEqual(16, result);
-        }
-
-        [Test]
-        //[ExpectedException(typeof(InvalidOperationException))]
-        public void TestSquareRoot()
-        {
-            var test = new Calculator.Calculator();
-            Assert.Throws<InvalidOperationException>(() => test.Squareroot(-4));
-
-        }
-    }
+		}
+	}
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,66 +2,66 @@
 
 namespace Calculator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Calculator calc = new Calculator();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculator calc = new Calculator();
 
-            double a = 10;
-            double b = 5;
+			double a = 10;
+			double b = 5;
 
-            double result1 = calc.Add(a, b);
-            double result2 = calc.Subract(a, b);
-            double result3 = calc.Multiply(a, b);
-            double result4 = calc.Power(a, b);
+			double result1 = calc.Add(a, b);
+			double result2 = calc.Subract(a, b);
+			double result3 = calc.Multiply(a, b);
+			double result4 = calc.Power(a, b);
 
-            Console.WriteLine($"Test of Add: {a} + {b} = {result1}");
-            Console.WriteLine($"Test of Subtract: {a} - {b} = {result2}");
-            Console.WriteLine($"Test of Multiply: {a} * {b} = {result3}");
-            Console.WriteLine($"Test of Power: ({a})^{b} = {result4}");
+			Console.WriteLine($"Test of Add: {a} + {b} = {result1}");
+			Console.WriteLine($"Test of Subtract: {a} - {b} = {result2}");
+			Console.WriteLine($"Test of Multiply: {a} * {b} = {result3}");
+			Console.WriteLine($"Test of Power: ({a})^{b} = {result4}");
 
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 
 
-    public class Calculator
-    {
-        public double Add(double a, double b)
-        {
-            return a + b;
-        }
+	public class Calculator
+	{
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
 
-        public double Subract(double a, double b)
-        {
-            return a - b;
-        }
+		public double Subract(double a, double b)
+		{
+			return a - b;
+		}
 
-        public double Multiply(double a, double b)
-        {
-            return a * b;
-        }
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
 
-        public double Power(double x, double exp)
-        {
-            return Math.Pow(x, exp);
-        }
+		public double Power(double x, double exp)
+		{
+			return Math.Pow(x, exp);
+		}
 
-        public double Divide(double a, double b)
-        {
-            if(b == 0)
-                throw new DivideByZeroException();
-            return a / b;
-        }
+		public double Divide(double a, double b)
+		{
+			if(b == 0)
+				throw new DivideByZeroException();
+			return a / b;
+		}
 
-        public double Squareroot(double a)
-        {
-            if (a < 0)
-                throw new InvalidOperationException();
-            return Math.Sqrt(a);
-        }
-    }
+		public double Squareroot(double a)
+		{
+			if (a < 0)
+				throw new InvalidOperationException();
+			return Math.Sqrt(a);
+		}
+	}
 
 
 
